37e83d9c6ccaadc1b01795c0acb425133304f90f
alison <adevpr11@gmail.com>
2025-09-05 20:57:28 -0300

    feat: estrutura inicial e testes de integração e unidade falhando

---
+++ src/test/java/br/com/advanced/integration/ClienteControllerTest.java
@@ -0,0 +1,33 @@
+package br.com.advanced.integration;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ClienteControllerTest {
+
+	@Test
+	@DisplayName("POST /api/v1/clientes deve retornar 201 ao cadastrar")
+	public void postClientesReturnsCreated() {
+		fail();
+	}
+	
+	@Test
+	@DisplayName("PATCH /api/v1/clientes deve retornar 200 ao atualizar")
+	public void patchClientesReturnsOk() {
+		fail();
+	}
+	
+	@Test
+	@DisplayName("DELETE /api/v1/clientes deve retornar 200 ao excluir")
+	public void deleteClientesReturnsOk() {
+		fail();
+	}
+	
+	@Test
+	@DisplayName("PUT /api/v1/clientes deve retornar 200 ao atualizar")
+	public void putClientesReturnsOk() {
+		fail();
+	}
+}
---
+++ src/test/java/br/com/advanced/unit/ClienteServiceTest.java
@@ -0,0 +1,21 @@
+package br.com.advanced.unit;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ClienteServiceTest {
+
+	@BeforeEach
+	public void setUp() {
+		
+	}
+	
+	@Test
+	@DisplayName("Deve salvar cliente")
+	public void deveSalvarCLiente() {
+		fail();
+	}
+}
