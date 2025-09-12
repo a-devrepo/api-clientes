65775f4d3c24890d9621c74bc226f2f4e3665b01
alison <adevpr11@gmail.com>
2025-09-12 20:35:15 -0300

    feat: adiciona query methods em ClienteRepository

--- src/main/java/br/com/advanced/infrastructure/repositories/ClienteRepository.java
+++ src/main/java/br/com/advanced/infrastructure/repositories/ClienteRepository.java
@@ -1,11 +1,26 @@
 package br.com.advanced.infrastructure.repositories;
 
+import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.advanced.domain.entities.Cliente;
 
+
 @Repository
-public interface ClienteRepository extends JpaRepository<Cliente,UUID> {}
+public interface ClienteRepository extends JpaRepository<Cliente,UUID> {
+	
+	boolean existsByCpf(String cpf);
+	
+	boolean existsByEmail(String email);
+	
+	Optional<Cliente> findByCpf(String cpf);
+	
+	Optional<Cliente> findByEmail(String email);
+	
+	Page<Cliente> findByAtivoTrue(Pageable pageable);
+}
